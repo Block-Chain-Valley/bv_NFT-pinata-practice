fdee12ba39c43d9301b2131a64af0174bad1c876
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-09-20 16:22:12 +0900

    Ad mintGenesisToken

--- scripts/mintGenesisNFT.ts
+++ scripts/mintGenesisNFT.ts
@@ -1,18 +1,42 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function mintmain() {
-  const [owner] = await ethers.getSigners();
-  const GenesisNFT = await ethers.getContractFactory("GenesisNFT");
-  const genesisNFT = GenesisNFT.attach("Your_Contract_Address_Here");
+  const provider = hre.ethers.provider; // Hardhat's built-in provider
+  const privateKey = process.env.SEPOLIA_PRIVATE_KEY as string; // 환경 변수의 타입을 명시적으로 지정
 
-  const tx = await genesisNFT
-    .connect(owner)
-    .mintNFT(
-      owner.address,
-      "ipfs://여기에는 genesis-metada를 pinata에 올렸을때 얻은 CID 입력"
+  if (!privateKey) {
+    console.error(
+      "SEPOLIA_PRIVATE_KEY is not set in the environment variables."
     );
-  await tx.wait();
-  console.log(`NFT minted with tokenId: ${tx.events[0].args.tokenId}`);
+    return;
+  }
+
+  const wallet = new hre.ethers.Wallet(privateKey, provider); // 연결된 프로바이더 추가
+
+  const GenesisNFT = await hre.ethers.getContractFactory("GenesisNFT");
+  const genesisNFT = GenesisNFT.attach(
+    "deployGenesisNFTContract.ts를_통해서_배포된_컨트랙트_주소" /////채워주세요~
+  );
+  try {
+    const tx = await genesisNFT.connect(wallet).mintNFT(
+      wallet.address,
+      "ipfs://여기에는_genesis-metada를_pinata에_올렸을때_얻은_CID_입력" /////채워주세요~
+    );
+    const receipt = await tx.wait();
+    const mintEvent = receipt.events?.find((e: any) => e.event === "Minted");
+    const tokenId = mintEvent?.args?.tokenId;
+
+    if (tokenId !== undefined) {
+      console.log(`NFT minted with tokenId: ${tokenId}`);
+    } else {
+      console.log("NFT minted, but tokenId is unknown");
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
 
 mintmain()
